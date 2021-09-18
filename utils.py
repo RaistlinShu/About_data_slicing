833a783775ecabd16a75a848e97b99f47f1d11f0
Fan Shu <54341901+rastlinshu@users.noreply.github.com>
2021-09-18 23:14:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -45,10 +45,10 @@ def create_bins(lower_bound, upper_bound, quantity):
     create bins for numeric
     """
     width = (upper_bound - lower_bound) / quantity
-    bins = set()
+    bins = list()
     for i in range(quantity - 1):
-        bins.add((int(lower_bound + i * width), int(lower_bound + (i + 1) * width)))
-    bins.add((int(lower_bound + (quantity - 1) * width), upper_bound + 1))
+        bins.append((int(lower_bound + i * width), int(lower_bound + (i + 1) * width)))
+    bins.append((int(lower_bound + (quantity - 1) * width), upper_bound + 1))
     return bins
 
 
@@ -59,24 +59,27 @@ def notfindother(values):
     return True
 
 
-def divide_slice(data):
+def divide_slice(data, min_slice_size=100, flag=True):
     res = {}
     data_size = data.shape[0]
-    size_threshold = data_size / 100
+    if flag:
+        size_threshold = data_size / min_slice_size
+    else:
+        size_threshold = 1
     for column in data:
         if str(data[column].dtype) == 'category':
-            values = set()
+            values = list()
             value_counts = data[column].value_counts()
             index = value_counts.index
             # print(index)
             flag = 0
             for i in range(len(index)):
                 if value_counts[index[i]] >= size_threshold:
-                    values.add(index[i])
+                    values.append(index[i])
                 else:
                     flag = 1
             if flag == 1 and notfindother(values=values):
-                values.add('Other')
+                values.append('Other')
             res[str(column)] = values
         else:
             res[str(column)] = create_bins(lower_bound=data[column].min(),
